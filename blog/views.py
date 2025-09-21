8b9e49fd1118c6bee8591a89827db9914b0bba37
YA <diplomaticmugfight8934@gmail.com>
2025-09-21 21:11:16 +0300

    Added home template and navbar + a little css

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home page<h1>')
+    return render(request, 'blog/home.html')
 
 
 def about(request):
-    return HttpResponse('<h1>About Page<h1>')
+    return render(request, 'blog/about.html')
--- django_blog_project/settings.py
+++ django_blog_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'django_blog_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
