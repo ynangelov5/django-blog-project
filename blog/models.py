a151664f96cb448309e5a74281fe03031f62a351
YA <diplomaticmugfight8934@gmail.com>
2025-09-21 21:46:25 +0300

    Added Post model and Timestamps mixin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
+from blog.mixins import Timestamps 
 
-# Create your models here.
+
+class Post(Timestamps):
+    title = models.CharField(
+        max_length=100, 
+        validators=[MinLengthValidator(2)]
+        )
+    content = models.TextField()
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name="posts"
+        )
+
+    def __str__(self):
+        return self.title
+    
+
+
