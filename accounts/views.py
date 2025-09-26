03956a09f92c7309df6485d39178eece0315d1b3
YA <diplomaticmugfight8934@gmail.com>
2025-09-26 22:33:16 +0300

    Fix typo

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login
-~from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserRegisterForm
 
 
