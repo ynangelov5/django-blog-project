8e04e0113c0185cf0549b91fb332f75014e10b80
YA <diplomaticmugfight8934@gmail.com>
2025-10-05 20:03:20 +0300

    Add profile pic resizing and made profile pic appear next to a user's post

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -8,7 +9,7 @@ class Profile(models.Model):
         on_delete=models.CASCADE
         )
     image = models.ImageField(
-        default='profile_pics/common/default.jpg',
+        default='common/default_profile_pic.jpg',
         upload_to='profile_pics'
         )
     bio = models.CharField(
@@ -19,4 +20,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+        max_size = (300, 300)
+
+        if img.width > max_size[0] or img.height > max_size[1]:
+            img.thumbnail(max_size)
+            img.save(self.image.path)
     
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from blog.models import Post
 
 
 def home(request):
     posts = Post.objects.all().order_by('-created_at')
+    # user = request.user.profile.image
     return render(request, 'blog/home.html', {'posts': posts})
 
 
