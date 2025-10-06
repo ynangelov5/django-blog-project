ed73f5c0f5116d3511ba7df763dde673d23eee95
YA <diplomaticmugfight8934@gmail.com>
2025-10-06 14:41:23 +0300

    Add CRUD operations for blog posts using class based views

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator
+from django.urls import reverse
 from blog.mixins import Timestamps 
 
 
@@ -18,6 +19,9 @@ class Post(Timestamps):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
     
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView, DetailView,
+    CreateView, UpdateView,
+    DeleteView
+    )
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin, UserPassesTestMixin
+    )
 from blog.models import Post
 
 
@@ -10,3 +18,55 @@ def home(request):
 
 def about(request):
     return render(request, 'blog/about.html')
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post-detail.html'
+    context_object_name = 'post'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post-create.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+     
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post-update.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author 
+        # raises 403 if a user tries to edit someone else's post
+
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post-delete.html'
+    context_object_name = 'post'
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author 
+        # raises 403 if a user tries to delete someone else's post
+
